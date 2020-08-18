4e63386e0156240bfd89b043dfac52fa9fe5312c
Callum Bartlett <callum@voxelfox.co.uk>
2020-08-18 15:52:39 +0100

    Catch herbiary command so as to list all plants in the bot

--- cogs/plant_info_commands.py
+++ cogs/plant_info_commands.py
@@ -24,8 +24,15 @@ class PlantInfoCommands(utils.Cog):
 
     @commands.command(cls=utils.Command, aliases=['describe', 'info', 'information'])
     @commands.bot_has_permissions(send_messages=True, embed_links=True, attach_files=True)
-    async def herbiary(self, ctx:utils.Context, *, plant_name:str):
+    async def herbiary(self, ctx:utils.Context, *, plant_name:str=None):
         """Get the information for a given plant"""
+
+        # See if a name was given
+        if plant_name is None:
+            with utils.Embed(use_random_colour=True) as embed:
+                plants = sorted(self.bot.plants.values(), key=lambda x: (x.plant_level, x.name))
+                embed.description = '\n'.join([f"**{i.display_name.capitalize()}** - level {i.plant_level}" for i in plants])
+            return await ctx.send(embed=embed)
 
         # See if the given name is valid
         plant_name = plant_name.replace(' ', '_').lower()
@@ -34,22 +41,28 @@ class PlantInfoCommands(utils.Cog):
         plant = self.bot.plants[plant_name]
 
         # Work out our artist info to be displayed
-        artist_info_embed_list = []
+        description_list = []
         artist_info = self.artist_info[plant.artist].copy()
         discord_id = artist_info.pop('discord', None)
         if discord_id:
             user = await self.bot.fetch_user(discord_id)
-            artist_info_embed_list.append(f"**Artist `{user!s}`**")
-            artist_info_embed_list.append(f"Discord: {user.mention}")
+            description_list.append(f"**Artist `{user!s}`**")
+            description_list.append(f"Discord: {user.mention}")
         else:
-            artist_info_embed_list.append(f"**Artist `{plant.artist}`**")
+            description_list.append(f"**Artist `{plant.artist}`**")
         for i, o in sorted(artist_info.items()):
-            artist_info_embed_list.append(f"{i.capitalize()}: [Link]({o})")
+            description_list.append(f"{i.capitalize()}: [Link]({o})")
+        description_list.append("")
+
+        # Work out some other info we want displayed
+        description_list.append(f"Plant level {plant.plant_level}")
+        description_list.append(f"Costs {plant.required_experience} exp")
+        description_list.append(f"Gives between {plant.experience_gain['minimum']} and {plant.experience_gain['maximum']} exp")
 
         # Embed the data
         with utils.Embed(use_random_colour=True) as embed:
             embed.title = plant.display_name.capitalize()
-            embed.description = '\n'.join(artist_info_embed_list)
+            embed.description = '\n'.join(description_list)
             embed.set_image("attachment://plant.png")
         display_cog = self.bot.get_cog("PlantDisplayCommands")
         plant_image_bytes = display_cog.image_to_bytes(display_cog.get_plant_image(plant.name, 0, 21, "clay", random.randint(0, 360)))
