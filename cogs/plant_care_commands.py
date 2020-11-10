531dfa104098bb1a6dcfecd3e77a3d362998a1ed
Callum Bartlett <callum@voxelfox.co.uk>
2020-11-10 12:52:01 +0000

    Set the embed footer at least once

--- cogs/plant_care_commands.py
+++ cogs/plant_care_commands.py
@@ -197,7 +197,7 @@ class PlantCareCommands(utils.Cog):
 
             # Add "please vote for Flower" footer
             counter = 0
-            embed.set_footer("")
+            ctx._set_footer(embed)
             check = lambda text: 'vote' in text if voted_on_topgg else False  # Return True to change again - force to "vote for flower" if they haven't voted, else anything but
             while counter < 100 and check(embed.footer.text.lower()):
                 ctx._set_footer(embed)
