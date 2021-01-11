e79c7a4a54ff377cfab200cd4fba4e952326923a
Callum Bartlett <callum@voxelfox.co.uk>
2021-01-11 23:17:02 +0000

    Add an inventory section to the flowers page

--- website/frontend.py
+++ website/frontend.py
@@ -37,14 +37,56 @@ async def flowers(request:Request):
     Show the users their flowers.
     """
 
+    # Get the ID of the user signed in
     session = await aiohttp_session.get_session(request)
-    try:
-        user_id = request.query.get('user_id', session['user_id'])
-        user_id = int(user_id)
-        if user_id != session['user_id'] and session['user_id'] not in request.app['bots']['bot'].config['owner_ids']:
-            raise ValueError()
-    except ValueError:
-        user_id = session['user_id']
+    user_id = session['user_id']
+
+    # Grab their plants
+    async with request.app['database']() as db:
+        user_rows = await db("SELECT * FROM user_settings WHERE user_id=ANY($1::BIGINT[]) ORDER BY user_id DESC LIMIT 1", [user_id, 0])
+        user_inventory_rows = await db("SELECT * FROM user_inventory WHERE user_id=$1 ORDER BY item_name ASC", user_id)
+        plant_rows = await db("SELECT * FROM plant_levels WHERE user_id=$1 AND amount > 0 ORDER BY plant_name ASC", user_id)
+    plants = [dict(i) for i in plant_rows]
+
+    # Generate the base64 image data for each of the plants
+    display_utils = request.app['bots']['bot'].get_cog("PlantDisplayUtils")
+    for data in plants:
+        plant_display_dict = display_utils.get_display_data(data, user_id=data['user_id'])
+        display_data = display_utils.get_plant_image(**plant_display_dict)
+        cropped_display_data = display_utils.crop_image_to_content(display_data)
+        image_bytes = display_utils.image_to_bytes(cropped_display_data)
+        data['image_data'] = base64.b64encode(image_bytes.read()).decode()
+
+    # Fix up the inventory dictionary
+    inventory = {i['item_name'].replace('_', ' ').title(): i['amount'] for i in user_inventory_rows}
+    inventory.setdefault('Revival Token'.title(), 0)
+
+    # Return data for the page
+    return {
+        'user': dict(user_rows[0]),
+        'plants': plants,
+        'inventory': inventory,
+    }
+
+
+@routes.get("/shop")
+@template("shop.html.j2")
+@webutils.requires_login()
+@webutils.add_discord_arguments()
+async def shop(request:Request):
+    """
+    Show the users their shop.
+    """
+
+    # Get the ID of the user signed in
+    session = await aiohttp_session.get_session(request)
+    user_id = session['user_id']
+
+    # Grab their plants
+    async with request.app['database']() as db:
+        user_rows = await db("SELECT * FROM user_settings WHERE user_id=ANY($1::BIGINT[]) ORDER BY user_id DESC LIMIT 1", [user_id, 0])
+        plant_rows = await db("SELECT * FROM plant_levels WHERE user_id=$1 ORDER BY plant_name ASC", user_id)
+    plants = [dict(i) for i in plant_rows]
 
     async with request.app['database']() as db:
         user_rows = await db("SELECT * FROM user_settings WHERE user_id=ANY($1::BIGINT[]) ORDER BY user_id DESC LIMIT 1", [user_id, 0])
