5f2acd9de99b577dc83a6aad569bc41552ec0ee2
Callum Bartlett <callum@voxelfox.co.uk>
2021-02-03 05:30:50 +0000

    Allow owners to purchase new plants whenever they want

--- cogs/plant_shop_commands.py
+++ cogs/plant_shop_commands.py
@@ -158,6 +158,7 @@ class PlantShopCommands(utils.Cog):
             plant_limit = 1
             last_plant_shop_time = dt(2000, 1, 1)
         can_purchase_new_plants = dt.utcnow() > last_plant_shop_time + timedelta(**self.bot.config.get('plants', {}).get('water_cooldown', {'minutes': 15}))
+        can_purchase_new_plants = can_purchase_new_plants or ctx.author.id in self.bot.owner_ids
         buy_plant_cooldown_delta = None
         if can_purchase_new_plants is False:
             buy_plant_cooldown_delta = utils.TimeValue(
