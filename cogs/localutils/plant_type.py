2ffc7b74cc5976eaf20c99e763e8c6344618b006
Kae Bartlett <Kae@voxelfox.co.uk>
2021-03-18 02:49:34 +0000

    Move our static variables into the class instance

--- cogs/localutils/plant_type.py
+++ cogs/localutils/plant_type.py
@@ -18,10 +18,14 @@ class PlantType(object):
         6: {"cost": 12_500, "experience_gain": {"maximum": 300, "minimum": 160}},
     }
 
+    required_experience = 0
+    experience_gain = {"maximum": 300, "minimum": 150}
+    max_nourishment_level = 21
+
+    __slots__ = ('name', 'available_variants', 'nourishment_display_levels', 'stages', 'soil_hue', 'visible', 'available', 'artist',)
+
     def __init__(self, name:str, soil_hue:int, visible:bool, available:bool, artist:str, stages:int=None, plant_level:int=None, nourishment_display_levels:dict=None, available_variants:dict=None):
         self.name = name
-        self.required_experience = 0
-        self.experience_gain = {"maximum": 300, "minimum": 150}
         self.available_variants = available_variants
         self.nourishment_display_levels = nourishment_display_levels if nourishment_display_levels else self.calculate_display_for_stages(stages)
         self.stages = stages if stages else len(nourishment_display_levels)
@@ -29,7 +33,6 @@ class PlantType(object):
         self.visible = visible
         self.available = available
         self.artist = artist
-        self.max_nourishment_level = 21  # max([int(i) for i in self.nourishment_display_levels.keys()]) + 1
 
     @staticmethod
     def calculate_display_for_stages(stages:int) -> dict:
