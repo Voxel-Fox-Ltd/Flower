63a9490ee023ba8bec10ff40dcb6ccdfb8ed8f4b
Kae Bartlett <kae@voxelfox.co.uk>
2022-04-18 15:29:31 +0100

    Add bot config for typing

--- cogs/utils/types/bot.py
+++ cogs/utils/types/bot.py
@@ -1,10 +1,29 @@
 import typing
 
 from discord.ext import vbu
+from voxelbotutils.cogs.utils.types.config import BotConfig
 
 from cogs import utils
+
+
+class _Plants(typing.TypedDict):
+    non_subscriber_plant_cap: int
+    hard_plant_cap: int
+    revival_token_price: int
+    refresh_token_price: int
+    immortal_plant_juice_price: int
+
+    death_timeout: dict
+    water_cooldown: dict
+    notification_time: dict
+    guest_water_cooldown: dict
+
+
+class _BotConfig(BotConfig):
+    plants: _Plants
 
 
 class Bot(vbu.Bot):
     plants: typing.Dict[str, utils.PlantType]
     items: typing.Dict[str, utils.ItemType]
+    config: _BotConfig
